cd4ec659ad48f996b0b6de697e26f3912b1540fc
Miguel Jimenez <miguelitojimenez291126@gmail.com>
2025-04-03 22:05:48 +0200

    fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,22 @@
+using Photon.Pun;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
     public SpriteRenderer spriterender;
     public Animator animator;
     public Rigidbody2D RB2d;
     public AudioSource audiosource;
+    public GameObject bullet;
 
     bool canjump;
     bool isinground;
     float speed = 5000f;
     float jumpForce = 2000f;
+    float BulletTime;
+    bool CanBullet;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,74 +30,76 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (!photonView.IsMine & !PhotonNetwork.OfflineMode) 
         {
-            RB2d.AddForce(new Vector2(speed * Time.deltaTime, 0));
-            animator.SetBool("moving", true);
-            spriterender.flipX = false;
-
-            if (!audiosource.isPlaying && isinground)
-            {
-                audiosource.Play();
-            }
-            
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            RB2d.AddForce(new Vector2(-speed * Time.deltaTime, 0 ));
-            animator.SetBool("moving", true);
-            spriterender.flipX = true;
-
-            if (!audiosource.isPlaying && isinground)
-            {
-                audiosource.Play();
-            }
+            return;
         }
 
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        float moveInput = Input.GetAxisRaw("Horizontal"); // Movimiento más preciso
+
+        // Mover personaje
+        RB2d.linearVelocity = new Vector2(moveInput * speed * Time.deltaTime, RB2d.linearVelocity.y);
+
+        // Control de animaciones
+        if (moveInput != 0)
+        {
+            animator.SetBool("moving", true);
+            spriterender.flipX = moveInput < 0; // Voltear el sprite dependiendo de la dirección
+        }
+        else
         {
             animator.SetBool("moving", false);
-
-            if (audiosource.isPlaying)
-            {
-                audiosource.Pause();
-            }
         }
 
-        if (audiosource.isPlaying && !isinground)
+        // Control del sonido al caminar
+        if (!audiosource.isPlaying && isinground && moveInput != 0)
+        {
+            audiosource.Play();
+        }
+        else if ((moveInput == 0 || !isinground) && audiosource.isPlaying)
         {
             audiosource.Pause();
         }
 
-        if (Input.GetKey(KeyCode.Space) && canjump)
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && canjump)
         {
             canjump = false;
+            isinground = false;
             animator.SetBool("jumping", true);
-            RB2d.AddForce(new Vector2(0, jumpForce));
+            RB2d.linearVelocity = new Vector2(RB2d.linearVelocity.x, jumpForce * Time.deltaTime); // Usar velocity en vez de AddForce
         }
 
-        if (gameObject.GetComponent<Rigidbody2D>().linearVelocity.y < 0)
+        if (Input.GetKeyDown(KeyCode.S) && CanBullet)
         {
-            animator.SetBool("jumping", false);
-            animator.SetBool("falling", true);
+            GameObject bulletIst = Instantiate(bullet, transform.position, transform.rotation)
+            bulletIst.RGB2d.
         }
-        else
+
+        // Detectar caída
+        if (RB2d.linearVelocity.y < -0.1f)
         {
+            animator.SetBool("falling", true);
+            animator.SetBool("jumping", false);
+        }
+        else if (RB2d.linearVelocity.y == 0)
+        {   
             animator.SetBool("falling", false);
         }
-
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "terrain")
+        if (collision.transform.CompareTag("terrain"))
         {
-            canjump = true;
             isinground = true;
-            animator.SetBool("falling", false);
+            canjump = true;
             animator.SetBool("jumping", false);
+            animator.SetBool("falling", false);
         }
     }
+
 
     private void OnCollisionExit2D(Collision2D collision)
     {
--- Assets/Scripts/ResetZone.cs
+++ Assets/Scripts/ResetZone.cs
@@ -4,10 +4,12 @@ public class ResetZone : MonoBehaviour
 {
     public Transform respawnPoint; // Punto donde reaparece el jugador
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Detectando objeto...");
         if (collision.CompareTag("Player")) // Asegúrate de que el jugador tenga el tag "Player"
         {
+            Debug.Log("¡Jugador ha tocado el trigger!");
             collision.transform.position = respawnPoint.position; // Mueve al jugador al punto de reinicio
         }
     }
